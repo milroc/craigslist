b2bfc0d49054c818ad2babf8d0c4ecf63a5554b2
Dan Cleary <dcc635@gmail.com>
2013-04-05 21:04:17 +0000

    Cleaned up pipelines.py

--- craigslist/pipelines.py
+++ craigslist/pipelines.py
@@ -9,27 +9,29 @@ class CraigslistPipeline(object):
 
     def __init__(self):
         self.persist_dict = shelve.open("craigslist_scrapy")
-        self.email_info = []
+        self.email_list = []
 
     def process_item(self, item, spider):
-        d_item = dict(item)
+        item_dict = dict(item)
         lookup_val = str(item["post_number"])
-        if lookup_val not in self.persist_dict:
-            self.persist_dict[lookup_val] = d_item
-            self.email_info.append("".join([str(item), "\n\n"]))
-        elif self.persist_dict[lookup_val] != d_item:
-            self.persist_dict[lookup_val] = d_item
-            self.email_info.append("".join([str(item), "\n\n"]))
+        if (
+            lookup_val not in self.persist_dict
+            or self.persist_dict[lookup_val] != item_dict
+        ):
+            self.persist_dict[lookup_val] = item_dict
+            self.email_list.append(str(item))
         return item
 
     def close_spider(self, spider):
-        self.email_info = "".join(self.email_info)
         self.persist_dict.close()
+        if not self.email_list:
+            return
+        email_str = "\n\n".join(self.email_list)
         mailer = MailSender.from_settings(spider.settings)
         with open('list.csv', 'r') as csv_file:
             mailer.send(
                 to = ["dcc635@gmail.com"],
                 subject = "Scrapy Info",
-                body = self.email_info,
+                body = email_str,
                 attachs = [('scrapy_info.csv', 'text/csv', csv_file)],
             )
